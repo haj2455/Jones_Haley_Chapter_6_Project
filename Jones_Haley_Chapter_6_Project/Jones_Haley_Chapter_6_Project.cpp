f96150d53c42b20429dc9096032e8c747372a0c4
haj2455 <haleybear057@gmail.com>
2025-07-23 19:43:52 -0400

    save

--- Jones_Haley_Chapter_6_Project/Jones_Haley_Chapter_6_Project.cpp
+++ Jones_Haley_Chapter_6_Project/Jones_Haley_Chapter_6_Project.cpp
@@ -2,19 +2,27 @@
 //
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int MIN_F = 0;
+const int MAX_F = 20;
+
+double getCelsius(int fahrenheit);
+
+int main() {
+	cout << "Temperature Conversion Table\n";
+	cout << setw(10) << "Fahrenheit" << setw(15) << "Celsius\n";
+	cout << "-----------------------------\n";
+
+	for (int f = MIN_F; f <= MAX_F; ++f) {
+		double c = getCelsius(f);
+		cout << setw(10) << f << setw(15) << fixed << setprecision(1) << c << endl;
+	}
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+double getCelsius(int fahrenheit) {
+	return (5.0 / 9.0) * (fahrenheit - 32);
+}
