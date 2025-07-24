4a34906bf2fb0425d213d76d37dc3314cf1b72d6
haj2455 <haleybear057@gmail.com>
2025-07-23 20:03:33 -0400

    save

--- Jones_Haley_Chapter_6_Project/Jones_Haley_Chapter_6_Project.cpp
+++ Jones_Haley_Chapter_6_Project/Jones_Haley_Chapter_6_Project.cpp
@@ -2,19 +2,34 @@
 //
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int NUM_JUDGES = 5;
+
+double getScore();
+
+bool isLower(double a, double b);
+
+bool isHigher(double a, double b);
+
+double calcAverage(double total, double minScore, double maxScore);
+
+int main() {
+	double score, total = 0.0;
+	double minScore, maxScore;
+
+	score = getScore();
+	total = score;
+	minScore = score;
+	maxScore = score;
+
+	for (int i = 1; i < NUM_JUDGES; i++) {
+		score = getScore();
+		total += score;
+
+		if (isLower(score, minScore)) {
+		}
+		if (isHigher(score, maxScore))
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
