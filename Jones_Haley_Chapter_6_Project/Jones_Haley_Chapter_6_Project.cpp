a7c002be7ab718856288ebf40e20b07c44a66ffc
haj2455 <haleybear057@gmail.com>
2025-07-23 20:56:47 -0400

    save

--- Jones_Haley_Chapter_6_Project/Jones_Haley_Chapter_6_Project.cpp
+++ Jones_Haley_Chapter_6_Project/Jones_Haley_Chapter_6_Project.cpp
@@ -15,6 +15,7 @@ bool isHigher(double a, double b);
 
 double calcAverage(double total, double minScore, double maxScore);
 
+// This program calculates a performer’s score. Five judges each contribute a score, the highest and lowest score is dropped and the remaining three are averaged
 int main() {
 	double score, total = 0.0;
 	double minScore, maxScore;
@@ -28,14 +29,18 @@ int main() {
 		score = getScore();
 		total += score;
 
+		// This function takes two integer values as input, it returns true if the first value is <= the second value, otherwise it returns false
 		if (isLower(score, minScore)) {
 			minScore = score;
 		}
+		// This function takes two integer values as input, it returns true if the first value is >= the second value, otherwise it returns false
 		if (isHigher(score, maxScore)) {
 			maxScore = score;
 		}
 	}
 
+	// This function takes as input the total of all the scores, the number of scores, the minimum score value and the maximum score value.
+	// It returns the average of the three middle scores by subtracting out the minimum and maximum scores from the total.
 	double average = calcAverage(total, minScore, maxScore);
 
 	cout << fixed << setprecision(2);
