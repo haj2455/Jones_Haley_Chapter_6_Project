bd5e6dbb8f81b7fda21a804375b6a7eea6877e44
haj2455 <haleybear057@gmail.com>
2025-07-23 21:11:46 -0400

    save

--- Jones_Haley_Chapter_6_Project/Jones_Haley_Chapter_6_Project.cpp
+++ Jones_Haley_Chapter_6_Project/Jones_Haley_Chapter_6_Project.cpp
@@ -2,19 +2,32 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const double SQFT_PER_GALLON = 110.0;
+const double LABOR_HOURS_PER_GALLON = 8.0;
+const double LABOR_RATE_PER_HOUR = 25.0;
+
+int getRooms();
+
+double getSqFt(int roomNum);
+
+double getPricePerGallon(int roomNum);
+
+int gallonsForRoom(double sqft);
+
+void displayEstimate(double paintCharge, int totalGallons, double laborCharge, double totalLaborHours);
+
+int main() {
+	int numRooms;
+	double totalPaintCharge = 0.0;
+	double totalLaborCharge = 0.0;
+	int totalGallons = 0;
+	double totalLaborHours = 0.0;
+
+	numRooms = getRooms();
+
+	for 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
